8e8a3011df6fb47af2bf48b2085c30518d366515
Bourbon <bourbon@jerrytekiMac.local>
2013-11-08 09:51:34 +0800

    Initial Commit

---
+++ iOS_NSString转换为16进制/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iOS_NSString转换为16进制
+//
+//  Created by Bourbon on 13-11-8.
+//  Copyright (c) 2013年 mamarow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iOS_NSString转换为16进制/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iOS_NSString转换为16进制
+//
+//  Created by Bourbon on 13-11-8.
+//  Copyright (c) 2013年 mamarow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
